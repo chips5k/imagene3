fe8cd85579d3cefee1496f4cab415eb3c411939d
chips5k <callem.pittard@myob.com>
2020-10-25 18:27:34 +1100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled, { createGlobalStyle, DefaultTheme, ThemeProvider }  from 'styled-components/macro'
+
+const darkMode: DefaultTheme = {
+  bg: "rgb(5, 21, 28)",
+  fg: "white",
+}
+
+const lightMode: DefaultTheme = {
+  bg: "rgb(255, 255, 255)",
+  fg: "black",
+}
+
+const GlobalStyle = createGlobalStyle`
+  body { 
+    margin: 0px;
+    background: ${props => props.theme.bg};
+    padding: 0px;
+  }
+`
+
+const Container = styled.div`
+  padding: 1em;
+  color: ${props => props.theme.fg};
+`
 
 function App() {
+  const [theme, setTheme] = useState(darkMode)
+  const toggleTheme = () => {
+    setTheme(theme == lightMode ? darkMode : lightMode)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <Container>        
+        Imagene3
+        <button onClick={toggleTheme}>Theme</button>
+      </Container>
+    </ThemeProvider>
   );
 }
+
 
 export default App;
